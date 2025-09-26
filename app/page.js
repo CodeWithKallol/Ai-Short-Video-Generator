1913840fef7941342163f31977f238fd3924e8d8
KALLOL BHATTACHARJEE <kallolbhattacharjee.2004@gmail.com>
2025-09-26 21:07:45 +0530

    Not a big change

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button"
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl text-orange-400">Welcome, Kallol Bhattacharjee</h1>
-      <Button variant="destructive">Submit</Button>
+      <Link href={'/dashboard'}>
+      <Button variant="destructive">Dashboard</Button>
+      </Link>
       <UserButton/>
     </div>
   );
